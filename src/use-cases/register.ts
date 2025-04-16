a23ba99480deefc88e22045416594a3fbde8c2ae
CaioDetore <caio.detore@sou.unifeob.edu.br>
2025-04-16 20:03:31 -0300

    refactor: inversao de dependencias

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -7,24 +7,27 @@ interface RegisterUseCaseRequest {
   email: string
   password: string
 }
-export async function registerUseCase({ email, name, password }: RegisterUseCaseRequest) {
-  const password_hash = await hash(password, 6)
 
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
+export class RegisterUseCase {
+  constructor(private usersRepository: any) { }
+
+  async execute({ email, name, password }: RegisterUseCaseRequest) {
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      }
+    })
+
+    if (userWithSameEmail) {
+      throw new Error('E-mail already exist.')
     }
-  })
 
-  if (userWithSameEmail) {
-    throw new Error('E-mail already exist.')
+    await this.usersRepository.create({
+      name,
+      email,
+      password_hash
+    })
   }
-
-  const prismaUserRepository = new PrismaUsersRepository()
-
-  prismaUserRepository.create({
-    name,
-    email,
-    password_hash
-  })
 }
