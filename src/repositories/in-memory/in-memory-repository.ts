1dbb41d5502496e388a3ad2e77e61191c000a9f2
CaioDetore <caio.detore@sou.unifeob.edu.br>
2025-04-18 11:40:37 -0300

    feat: teste de registro e  In-Memory Databases

---
+++ src/repositories/in-memory/in-memory-repository.ts
@@ -0,0 +1,28 @@
+import { UsersRepository } from '../user-repository'
+import { Prisma, User } from 'generated/prisma'
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public items: User[] = []
+
+  async findByEmail(email: string) {
+    const user = this.items.find(item => item.email === email)
+
+    if (!user) { return null }
+
+    return user
+  }
+
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    const user = {
+      id: 'user-1',
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    }
+
+    this.items.push(user)
+
+    return user
+  }
+}
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,7 +1,7 @@
-import { prisma } from "@/lib/prisma";
 import { UsersRepository } from "@/repositories/user-repository";
 import { hash } from "bcryptjs";
 import { UserAlreadyExistError } from "./errors/user-already-exist-error";
+import { User } from "generated/prisma";
 
 interface RegisterUseCaseRequest {
   name: string
@@ -9,10 +9,13 @@ interface RegisterUseCaseRequest {
   password: string
 }
 
+interface RegisterUseCaseResponse {
+  user: User
+}
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) { }
 
-  async execute({ email, name, password }: RegisterUseCaseRequest) {
+  async execute({ email, name, password }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
@@ -21,10 +24,14 @@ export class RegisterUseCase {
       throw new UserAlreadyExistError()
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash
     })
+
+    return {
+      user
+    }
   }
 }
