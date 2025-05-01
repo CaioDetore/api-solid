b1595fd058a25797e90112c0b881156d364e753c
CaioDetore <caio.detore@sou.unifeob.edu.br>
2025-05-01 13:25:54 -0300

    feat: controllers de academia

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,29 @@
+import { makeCreateGymUseCase } from "@/use-cases/factories/make-create-gym-use-case";
+import { makeFetchNearbyGymsUseCase } from "@/use-cases/factories/make-fetch-nearby-gyms";
+import { makeSearchGymsUseCase } from "@/use-cases/factories/make-search-gyms-use-case";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const nearbyGymsQuerySchema = z.object({
+    latitude: z.number().refine(value => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine(value => {
+      return Math.abs(value) <= 180
+    })
+  });
+
+  const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.body);
+
+  const fetchNearbyGymsUseCase = makeFetchNearbyGymsUseCase()
+
+  const { gyms } = await fetchNearbyGymsUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude
+  })
+
+  return reply.status(200).send({
+    gyms
+  });
+}
