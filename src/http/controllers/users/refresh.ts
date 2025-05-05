e9e2fdd800a3559c736cfe10b6c99a05126464a1
CaioDetore <caio.detore@sou.unifeob.edu.br>
2025-05-05 18:47:48 -0300

    feat: implementação do refresh token

--- src/http/controllers/users/refresh.ts
+++ src/http/controllers/users/refresh.ts
@@ -1,30 +1,21 @@
 import { InvalidCredentialsError } from "@/use-cases/errors/invalid-credentials-error";
-import { makeAuthenticateUseCase } from "@/use-cases/factories/make-authenticate-use-case";
 import { FastifyReply, FastifyRequest } from "fastify";
-import { z } from "zod";
 
-export async function authenticate(request: FastifyRequest, reply: FastifyReply) {
-  const authenticateBodySchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(6),
-  });
-
-  const { email, password } = authenticateBodySchema.parse(request.body);
+export async function refresh(request: FastifyRequest, reply: FastifyReply) {
+  await request.jwtVerify({
+    onlyCookie: true
+  })
 
   try {
-    const authenticateUseCase = makeAuthenticateUseCase()
-
-    const { user } = await authenticateUseCase.execute({ email, password })
-
     const token = await reply.jwtSign({}, {
       sign: {
-        sub: user.id
+        sub: request.user.sub
       }
     })
 
     const refreshToken = await reply.jwtSign({}, {
       sign: {
-        sub: user.id,
+        sub: request.user.sub,
         expiresIn: '7d'
       }
     })
