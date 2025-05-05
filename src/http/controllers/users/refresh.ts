80921f568a3fcea80727ba9bf1d1c2063ce1f48a
CaioDetore <caio.detore@sou.unifeob.edu.br>
2025-05-05 19:03:11 -0300

    feat: autorização por cargos (RBAC)

--- src/http/controllers/users/refresh.ts
+++ src/http/controllers/users/refresh.ts
@@ -6,14 +6,18 @@ export async function refresh(request: FastifyRequest, reply: FastifyReply) {
     onlyCookie: true
   })
 
+  const { role } = request.user
+
   try {
-    const token = await reply.jwtSign({}, {
+    const token = await reply.jwtSign({
+      role
+    }, {
       sign: {
         sub: request.user.sub
       }
     })
 
-    const refreshToken = await reply.jwtSign({}, {
+    const refreshToken = await reply.jwtSign({ role }, {
       sign: {
         sub: request.user.sub,
         expiresIn: '7d'
